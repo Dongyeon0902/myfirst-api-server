db663eba48979060d4f286f2a2b3236cdfde59f3
Dongyeon0902 <0902sdy@sch.ac.kr>
2025-09-19 11:39:16 +0900

    updates 2

--- devops/04.jenkins/createcm.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-NAME=sk199
-SERVICE_NAME="custom-jenkins"
-
-kubectl create configmap ${NAME}-${SERVICE_NAME}-config \
-  --from-file=jenkins.yaml=jenkins.yaml \
-  --namespace=skala-practice
